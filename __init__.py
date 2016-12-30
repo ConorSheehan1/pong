20f31e4755438e5e4601845d8e3219ca7c6b67a8
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-30 21:53:44 +0000

    starting template

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html", title="Pong")
+    return render_template("index.html", title="Pong", win_point=2)
+    # return render_template("template.html", score1=0)
 
 if __name__ == "__main__":
     app.debug = True
