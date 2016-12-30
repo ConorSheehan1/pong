e3d7e00b46aacb758902a5d8200503f27acc4208
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2016-12-30 22:27:52 +0000

    base template started, redirect working

--- __init__.py
+++ __init__.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("index.html", title="Pong", win_point=2)
+    return render_template("game.html", title="game", win_point=2)
     # return render_template("template.html", score1=0)
+
+
+@app.route('/leader_board')
+def leader_board():
+    return render_template("leader_board.html", title="leader board", win_point=2)
 
 if __name__ == "__main__":
     app.debug = True
