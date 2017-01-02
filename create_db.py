3439d235416633704c8b4b68490c34da6b0820be
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-02 21:36:14 +0000

    remove pop ups and redundant submit page

--- create_db.py
+++ create_db.py
@@ -2,16 +2,16 @@ import sqlite3
 conn = sqlite3.connect('pong.db')
 c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS leaderboard"
-          "(user_name text, score integer)")
+          "(user_name TEXT PRIMARY KEY, score INTEGER)")
 
 c.execute("CREATE TABLE IF NOT EXISTS users"
-          "(user_name text, password text, verified integer)")
+          "(user_name TEXT PRIMARY KEY, password TEXT, verified INTEGER)")
 
 # c.execute("INSERT INTO users "
 #           "values (?, ?, ?)", ("me", "me_irl", 0))
 
 c.execute("INSERT INTO leaderboard "
-          "values (?, ?)", ("me", 0))
+          "values (?, ?)", ("me", 100))
 
 conn.commit()
 conn.close()
