3c6b0c13c712982bcc3e32b6fb33fa7a3da1d6e4
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-02 22:26:51 +0000

    leaderboard highlight working

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from flask_heroku import Heroku
 from forms import SubmitForm
 import sqlite3
@@ -33,11 +33,17 @@ def game_page():
     form = SubmitForm()
     if form.validate_on_submit():
         submitted = submit_score(request.form["username"], request.form["score"])
-        print(submitted)
+
+        # if value submits to database, redirect to leaderboard and highlight row added
         if submitted:
-            flash("Your score" + request.form["score"] + "was submitted successfully")
+            conn = sqlite3.connect('pong.db')
+            c = conn.cursor()
+            l = [val for val in c.execute("SELECT * FROM leaderboard ORDER BY score DESC")]
+            return render_template("leader_board.html", title="Leader Board", leaderboard=l,
+                            highlight=request.form["username"])
+
         else:
-            flash("something went wrong")
+            flash("submission failed")
 
     return render_template("game.html", title="Game", win_point=1, form=form)
 
@@ -47,7 +53,7 @@ def leader_board():
     conn = sqlite3.connect('pong.db')
     c = conn.cursor()
     l = [val for val in c.execute("SELECT * FROM leaderboard ORDER BY score DESC")]
-    return render_template("leader_board.html", title="Leader Board", leaderboard=l)
+    return render_template("leader_board.html", title="Leader Board", leaderboard=l, highlight="")
 
 if __name__ == "__main__":
     app.run()
